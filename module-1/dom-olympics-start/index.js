a1c92471e778650895dea5da5f5164622207649a
Mia Stroud <miast36@gmail.com>
2020-06-12 16:46:44 -0400

    finished bronze

--- module-1/dom-olympics-start/index.js
+++ module-1/dom-olympics-start/index.js
@@ -1,3 +1,4 @@
+//qualifier
 document.querySelector("#header").classList.add("header")
 var headerTitle = document.getElementById("header")
 
@@ -18,4 +19,24 @@ subTitle.appendChild(namePart)
 var headerPart = document.createElement("span")
 headerPart.textContent = "wrote the JavaScript"
 subTitle.appendChild(headerPart)
+
+//bronze
+var leftMessage = document.querySelectorAll(".left")
+console.log(Array.from(leftMessage))
+leftMessage[0].textContent = "Hello :)"
+leftMessage[1].textContent = "I like hamsters"
+
+var rightMessage = document.querySelectorAll(".right")
+rightMessage[0].textContent = "Hi!"
+rightMessage[1].textContent = "Me too. We have so much in common"
+
+var main = document.querySelector("#main")
+var messages = document.querySelector(".messages")
+var messages2 = document.querySelector(".message")
+
+var clearButton = document.querySelector("#clear-button")
+clearButton.addEventListener("click", function() {
+    messages.remove(messages2)
+})
+
 
