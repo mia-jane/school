aa727ce4cc9e5141532cae56f73ca2af92c1ba41
Mia Stroud <miast36@gmail.com>
2020-06-18 14:08:32 -0400

    complete array practice

---
+++ module-1/array-methods-exercise/app.js
@@ -0,0 +1,31 @@
+var fruit = ["banana", "apple", "orange", "watermelon"]
+var vegetables = ["carrot", "tomato", "pepper", "lettuce"]
+
+vegetables.pop()
+console.log(vegetables)
+
+fruit.shift()
+console.log(fruit)
+
+var orangeIndex = fruit.indexOf("orange")
+console.log(orangeIndex)
+
+fruit.push("1")
+console.log(fruit)
+
+console.log(vegetables.length)
+
+vegetables.push("3")
+console.log(vegetables)
+
+var food = fruit.concat(vegetables)
+console.log(food)
+
+food.splice(4,2)
+console.log(food)
+
+food.reverse()
+console.log(food)
+
+var stringFood = food.join(",")
+console.log(stringFood)
