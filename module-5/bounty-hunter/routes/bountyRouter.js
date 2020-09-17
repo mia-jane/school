6e71f95bb9a7106762e9d10289912913cca22dc1
Mia Stroud <miast36@gmail.com>
2020-09-17 16:29:03 -0400

    put type fixed now lol

--- module-5/bounty-hunter/routes/bountyRouter.js
+++ module-5/bounty-hunter/routes/bountyRouter.js
@@ -6,16 +6,20 @@ const bounties = [
     {firstName: "Darth", lastName:"Vader", living: true, bountyAmount: 10000, type: "Sith", _id:uuidv4()}
 ]
 
+//get works
 bountyRouter.get("/", (req,res) => {
     res.send(bounties)
 })
 
+
+//post works
 bountyRouter.post("/", (req,res) => {
     const newBounty = req.body
     newBounty._id = uuidv4()
     bounties.push(newBounty)
     res.send("successfully added a new bounty")
 })
+
 
 bountyRouter.delete("/:bountyId", (req, res) => {
     // console.log(req.params.bountyId)
@@ -25,10 +29,13 @@ bountyRouter.delete("/:bountyId", (req, res) => {
     res.send("deleted bounty")
 })
 
-bountyRouter.put(":/bountyId", (req,res) => {
+bountyRouter.put("/:bountyId", (req,res) => {
     const bountyId = req.params.bountyId
     const bountyIndex = bounties.findIndex(bounty => bounty._id === bountyId)
-    
+    console.log(bountyIndex)
+    const updatedBounty = Object.assign(bounties[bountyIndex], req.body)
+    console.log(updatedBounty)
+    res.send(updatedBounty)
 })
 
 module.exports = bountyRouter
