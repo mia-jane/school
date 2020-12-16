3fa880cca8be6c2e990e6b646652dda964b96974
Mia Stroud <miast36@gmail.com>
2020-12-16 15:10:48 -0500

    made better

--- module-2/array-map-exercises/app.js
+++ module-2/array-map-exercises/app.js
@@ -1,21 +1,3 @@
-//1
-const numbers = [2, 5, 100]
-const doubleNumbers = numbers.map(function(num){
-    return num * 2
-})
-console.log(doubleNumbers)
-//2
-const numberStrings = numbers.map(function(num){
-    return num.toString()
-})
-console.log(numberStrings)
-//3
-const names = ["john", "JACOB", "jinGleHeimer", "schmidt"]
-const capitalizedNames = names.map(function(name){
-    return name.toUpperCase()
-})
-console.log(capitalizedNames)
-//4
 const users = [
     {
         name: "Angelina Jolie",
@@ -38,28 +20,55 @@ const users = [
         age: 100
     }
 ]
-const namesOnly = users.map(function(user){
-    return user.name
-})
-console.log(namesOnly)
-//5
-const goToTheMatrix = users.map(function(user){
-    if (user > 18){
-        return user.name + " can go to The Matrix"
-    } else {
-        return user.name + " is under age"
-    }
-})
-console.log(goToTheMatrix)
-//6
-const inElements = users.map(function(user){
-    const nameElement = document.createElement("h1")
-    const test = nameElement.textContent = user.name
-    document.body.append(nameElement)
-    const ageElement = document.createElement("h2")
-    const test2 = ageElement.textContent = user.age
-    document.body.append(ageElement)
-    console.log(nameElement)
-    return nameElement + ageElement
-})
-console.log(inElements)
+
+
+//1
+const doubleNumbers = (...numbers) => {
+    return numbers.map(n => n * 2)
+}
+console.log(doubleNumbers(1, 2, 3))
+
+
+//2
+const stringifyNumbers = (...numbers) => {
+    return numbers.map(n => n.toString())
+}
+console.log(stringifyNumbers(1, 2, 3))
+
+
+//3
+const capitalize = ([...names]) => {
+    return names.map(name => name.charAt(0).toUpperCase() + name.slice(1))
+}
+console.log(capitalize(["john", "alice"]))
+
+
+//4
+const namesOnly = (arr) => {
+    return arr.map(user => user.name)
+}
+console.log(namesOnly(users))
+
+// //5
+const canGoToMatrix = (arr) => {
+    return arr.map(user => user.age > 18 ? `${user.name} can go to the matrix` : `${user.name} is underage`)
+}
+console.log(canGoToMatrix(users))
+
+// //6
+const readyToPutInDom = (arr) => {
+    return arr.map(user => `<h1>${user.name}</h1><h2>${user.age}</h2>`)
+}
+console.log(readyToPutInDom(users))
+
+
+const inDom = (arr) => {
+    return arr.map(user => {
+        const username = document.createElement("h1").textContent = user.name + " "
+        document.body.append(username)
+        const age = document.createElement("h1").textContent = user.age + " "
+        document.body.append(age)
+    })
+}
+
+inDom(users)
