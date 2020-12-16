861f501658616f19441b4e883d7f5a3d6473e181
Mia Stroud <miast36@gmail.com>
2020-12-16 15:46:43 -0500

    much cleaner

--- module-2/array-filter-exercises/app.js
+++ module-2/array-filter-exercises/app.js
@@ -1,27 +1,5 @@
 const numbers = [3, 6, 8, 2]
-//1
-const fiveAndGreaterOnly = numbers.filter(function(num){
-    if(num >= 5){
-        return true
-    }
-})
-console.log(fiveAndGreaterOnly)
-//2
-const evenOnly = numbers.filter(function(num){
-    if(num % 2 === 0){
-        return true
-    }
-})
-console.log(evenOnly)
-//3
 const strings = ["dog", "wolf", "by", "family", "eaten", "camping"]
-const fiveCharactersOrFewer = strings.filter(function(string){
-    if(string.length <= 5){
-        return true
-    }
-})
-console.log(fiveCharactersOrFewer)
-//4
 const people = [
     { name: "Angelina Jolie", member: true, age: 80 },
     { name: "Eric Jones", member: false, age: 2 },
@@ -29,16 +7,28 @@ const people = [
     { name: "Kayne West", member: false, age: 16 },
     { name: "Bob Ziroll", member: true, age: 100 }
 ]
-const illuminatiClub = people.filter(function(person){
-    if(person.member === true){
-        return true
-    }
-})
-console.log(illuminatiClub)
+//1
+const greaterThanFive = (arr) => {
+    return arr.filter(n => n >= 5)
+}
+console.log(greaterThanFive(numbers))
+//2
+const evens = (arr) => {
+    return arr.filter(n => n % 2 === 0)
+}
+console.log(evens(numbers))
+//3
+const fiveCharactersAndFewer = (arr) => {
+    return arr.filter(n => n.length <= 5)
+}
+console.log(fiveCharactersAndFewer(strings))
+// //4
+const illuminatiClub = (arr) => {
+    return arr.filter(n => n.member)
+}
+console.log(illuminatiClub(people))
 //5
-const oldEnough = people.filter(function(person){
-    if(person.age > 18){
-        return true
-    }
-})
-console.log(oldEnough)
+const oldEnough = (arr) => {
+    return arr.filter(n => n.age > 18)
+}
+console.log(oldEnough(people))
